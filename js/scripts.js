95d58bc22d66a7c036a28ef4f2543b064c052481
Dan1elChaves-coder <danielchaves98@hotmail.com>
2025-11-04 23:46:29 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
-// scripts.js
-document.addEventListener('input',e=>{
- const m=(v,p)=>v.replace(/\D/g,'').replace(p.find,a=>p.repl(a));
- if(e.target.name==='cpf')e.target.value=m(e.target.value,{find:(v)=>v.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/,'$1.$2.$3-$4'),repl:x=>x});
- if(e.target.name==='tel')e.target.value=m(e.target.value,{find:(v)=>v.replace(/(\d{2})(\d{5})(\d{4})/,'($1) $2-$3'),repl:x=>x});
- if(e.target.name==='cep')e.target.value=m(e.target.value,{find:(v)=>v.replace(/(\d{5})(\d{3})/,'$1-$2'),repl:x=>x});
+// animação de fade ao carregar
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".card").forEach((el, i) => {
+    el.style.animationDelay = `${i * 0.2}s`;
+  });
+});
+
+// máscara CPF
+document.getElementById("cpf")?.addEventListener("input", function () {
+  let v = this.value.replace(/\D/g, "");
+  v = v.replace(/(\d{3})(\d)/, "$1.$2");
+  v = v.replace(/(\d{3})(\d)/, "$1.$2");
+  v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  this.value = v;
+});
+
+// máscara telefone
+document.getElementById("telefone")?.addEventListener("input", function () {
+  let v = this.value.replace(/\D/g, "");
+  v = v.replace(/(\d{2})(\d)/, "($1) $2");
+  v = v.replace(/(\d{5})(\d)/, "$1-$2");
+  this.value = v;
+});
+
+// máscara CEP
+document.getElementById("cep")?.addEventListener("input", function () {
+  let v = this.value.replace(/\D/g, "");
+  v = v.replace(/(\d{5})(\d)/, "$1-$2");
+  this.value = v;
 });
